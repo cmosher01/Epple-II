fd2aadfd17fa9139aaef14555a0d8c77dffc5bd9
Christopher A. Mosher <cmosher01@gmail.com>
2023-01-05 07:10:52 -0500

    add gtk to linux build setup

--- bin/Linux/setup.sh
+++ bin/Linux/setup.sh
@@ -6,5 +6,5 @@ echo "::endgroup::"
 
 echo "::group::install dependencies"
 sudo apt-get -qqqq update
-sudo apt-get -qqqq install libboost-all-dev libwxgtk3.2-dev libsdl2-dev
+sudo apt-get -qqqq install libboost-all-dev libwxgtk3.2-dev libsdl2-dev libgtk-3-dev
 echo "::endgroup::"
